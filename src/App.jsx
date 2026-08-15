45c352a66da23eb7a42e995970814d21e607db37
Abolfazl <abolfazlojaghi992@gmail.com>
2026-08-15 19:51:53 +0330

    add global skeleton theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { RouterProvider } from "react-router"
 import routes from "./routes"
 import { ThemeProvider } from "./context/ThemeContext"
+import { SkeletonTheme } from "react-loading-skeleton"
 const App = () => {
   return (
-    <ThemeProvider><RouterProvider router={routes}/></ThemeProvider>
+      <ThemeProvider>
+      <SkeletonTheme
+        baseColor="#6a7282"
+        highlightColor="#f3f4f6"
+      >
+        <RouterProvider router={routes} />
+      </SkeletonTheme>
+    </ThemeProvider>
   )
 }
 export default App
