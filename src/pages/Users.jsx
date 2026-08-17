f9953f286d709c38a77098d825dc2d016c03a911
Abolfazl <abolfazlojaghi992@gmail.com>
2026-08-17 13:15:01 +0330

    add search for users

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,11 +1,29 @@
 import { useFetch } from "../hooks/useFetch";
 import { getUsers } from "../services/requests/users";
 import UsersRow from "../components/UserRow";
+import { useState, useEffect } from "react";
 const Users = () => {
-  const { data: users } = useFetch(getUsers, ["users"]);
+  const [searchValue, setSearchValue] = useState();
+  const [debouncedSearch, setDebouncedSearch] = useState("");
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedSearch(searchValue);
+    }, 700);
+    return () => clearTimeout(timer);
+  }, [searchValue]);
+  const { data: users } = useFetch(() => getUsers(debouncedSearch), ["users", debouncedSearch]);
   return (
     <div className="page space-y-6">
       <h3>Users</h3>
+      <div className="bg-zinc-900 rounded-lg py-4 px-8">
+        <input
+          type="text"
+          className="px-6 py-2 rounded-xl focus:outline-none dark:bg-zinc-950 bg-gray-100 w-80 font-semibold"
+          placeholder="Search Users..."
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </div>
       <div>
         {users?.users.map((user) => {
           return (
--- src/services/requests/users.js
+++ src/services/requests/users.js
@@ -1,7 +1,8 @@
 import apiRequests from "../config/axios"
 
-export const getUsers = async () => {
-    const { data } = await apiRequests("/users?limit=0")
+export const getUsers = async (search = "") => {
+    const url = search ?  `/users/search?q=${search}` : "/users?limit=0"
+    const { data } = await apiRequests(url)
     return data  
 }
 export const getUserById = async (userId) => {
