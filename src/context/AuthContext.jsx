2f5d36054e667f18e88e8a62737685692da14471
Abolfazl <abolfazlojaghi992@gmail.com>
2026-08-31 13:37:02 +0330

    fix user authentication persistence across page refreshes

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,46 +3,62 @@ import { createContext, useState } from "react";
 import { getMe, loginRequest } from "../services/requests/auth";
 import { useEffect } from "react";
 const AuthContext = createContext();
+
 const AuthProvider = ({ children }) => {
   const queryClient = useQueryClient();
-  const [user, setUser] = useState(null);
   const [token, setToken] = useState(() => localStorage.getItem("accessToken"));
-  const { data, isLoading, isError } = useQuery({
+
+  const {
+    data: user,
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ["auth-me"],
     queryFn: getMe,
     enabled: !!token,
     retry: false,
   });
-  useEffect(() => {
-    data && setUser(data);
-  }, [data]);
+
   useEffect(() => {
     if (isError) {
       localStorage.removeItem("accessToken");
       localStorage.removeItem("refreshToken");
       setToken(null);
-      setUser(null);
+      queryClient.removeQueries({ queryKey: ["auth-me"] });
     }
   }, [isError]);
+
   const login = async (userData) => {
     const result = await loginRequest(userData);
     localStorage.setItem("accessToken", result.accessToken);
     localStorage.setItem("refreshToken", result.refreshToken);
     setToken(result.accessToken);
-    setUser(result);
-    queryClient.invalidateQueries({ queryKey: ["auth-me"] });
-    return result
+    queryClient.setQueryData(["auth-me"], result);
+    return result;
   };
+
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    setToken(null);
+    queryClient.removeQueries({ queryKey: ["auth-me"] });
+  };
+
+  const isCheckingAuth = !!token && isLoading;
+
   return (
     <AuthContext.Provider
       value={{
         user,
         token,
         login,
-        isLoading,
-        isLogin : !!user
+        logout,
+        isLoading: isCheckingAuth,
+        isLogin: !!user,
       }}
-    >{children}</AuthContext.Provider>
+    >
+      {children}
+    </AuthContext.Provider>
   );
 };
 export { AuthProvider, AuthContext };
