4e2176d78858a7b3def1716d6398addfb3a570ff
Abolfazl <abolfazlojaghi992@gmail.com>
2026-09-20 15:51:18 +0330

    fix unknown id problem in reviews

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -31,7 +31,7 @@ const {
   return (
     <div className="rounded-xl max-sm:p-3 px-5 py-4 dark:bg-zinc-900 bg-gray-50 border border-transparent dark:border-zinc-800 space-y-3">
       <div className="flex items-start justify-between gap-x-4">
-        <div className="flex items-center gap-x-3 cursor-pointer" onClick={() => navigate(`/users/${userId}`)}>
+        <div className="flex items-center gap-x-3 cursor-pointer" onClick={() => !review && navigate(`/users/${userId}`)}>
           <div className="bg-blue-600/10 text-blue-600 rounded-full p-1.5">
             <CircleUserRound size={28} />
           </div>
